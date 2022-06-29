3e05f1e2b06b867bfa12eb2e63f976d93d7fdccd
burhan <burabbani007@gmail.com>
2022-06-29 16:34:30 +0530

    initial commit

---
+++ chart2.js
@@ -0,0 +1,59 @@
+Highcharts.chart("container2", {
+  title: {
+    text: null,
+  },
+
+  credits: {
+    text: "created by",
+    href: "https://www.linkedin.com/in/burhan-mansoori-4b0256187/",
+    position: {
+      align: "left",
+      x: "10",
+    },
+    style: {
+      color: "red",
+      fontSize: "14px",
+      fontWeight: "bolder",
+      textDecoration: "underline",
+    },
+  },
+
+  yAxis: {
+    title: {
+      text: null,
+    },
+  },
+  /////// secondry YAxis //////
+
+  xAxis: {
+    categories: [
+      "26-may",
+      "27-may",
+      "28-may",
+      "29-may",
+      "30-may",
+      "31-may",
+      "01-jun",
+      "02-jun",
+      "03-jun",
+      "04-jun",
+      "05-jun",
+      "06-jun",
+      "07-jun",
+      "08-jun",
+      "09-jun",
+    ],
+  },
+  series: [
+    {
+      type: "spline",
+      name: "Express Adoption Rate",
+      data: [60, 40, 60, 90, 85, 40, 95, 50, 82, 50, 15, 34, 70, 55, 10],
+      marker: {
+        lineWidth: 2,
+        lineColor: Highcharts.getOptions().colors[3],
+        fillColor: "black",
+      },
+    },
+  ],
+});
